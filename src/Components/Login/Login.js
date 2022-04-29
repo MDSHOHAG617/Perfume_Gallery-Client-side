3092df7913a900bc6a5425bdc1d3a061f01b5731
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-29 18:27:14 +0600

    login and regi done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,28 +1,72 @@
-import { Button } from "bootstrap";
-import React from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import app from "../../firebase.init";
 
 const Login = () => {
+  const auth = getAuth(app);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Signed In User: {user.email}</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Form className="container w-50 mt-5 bg-light p-5 rounded">
         <h1 className="mb-5">Please Login</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
         </Form.Group>
         <Link className="text-decoration-none " to="/registration">
           <span>New to perfume gallery?</span>{" "}
         </Link>
         <br />
-        <button className="btn btn-primary mt-3" type="submit">
+        <button
+          onClick={() => signInWithEmailAndPassword(email, password)}
+          className="btn btn-primary mt-3"
+          type="submit"
+        >
           Login
         </button>
       </Form>
--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,27 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
+import app from "../../firebase.init";
 
 const Registration = () => {
+  const auth = getAuth(app);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
+
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Registered User: {user.email}</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Form className="container w-50 mt-5 bg-light p-5 rounded">
         <h1 className="mb-5">Please Registration</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
         </Form.Group>
         <Link className="text-decoration-none" to="/login">
           <span>All ready register?</span>{" "}
         </Link>
         <br />
-        <button className="btn btn-primary mt-3" type="submit">
+        <button
+          onClick={() => createUserWithEmailAndPassword(email, password)}
+          className="btn btn-primary mt-3"
+          type="submit"
+        >
           Register
         </button>
       </Form>
