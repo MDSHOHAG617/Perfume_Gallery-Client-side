3b4acda24570d4817583113e6dc618b727e24cdd
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-29 16:46:01 +0600

    login processing

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,52 @@
-import { Button } from "bootstrap";
-import React from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
+import auth from "../../firebase.init";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Signed In User: {user.email}</p>
+      </div>
+    );
+  }
+
+  const handleLogin = (e) => {};
   return (
     <div>
-      <Form className="container w-50 mt-5 bg-light p-5 rounded">
+      <Form
+        onSubmit={handleLogin}
+        className="container w-50 mt-5 bg-light p-5 rounded"
+      >
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -16,12 +54,22 @@ const Login = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
-        <button variant="primary" className="btn btn-" type="submit">
+        <button
+          onClick={() => signInWithEmailAndPassword(email, password)}
+          variant="primary"
+          className="btn btn-"
+          type="submit"
+        >
           Submit
         </button>
       </Form>
---
+++ src/firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD73cbGVRj06-x0eOSJhrjYKm70__-00RE",
+  authDomain: "assignment-11-node-mongo.firebaseapp.com",
+  projectId: "assignment-11-node-mongo",
+  storageBucket: "assignment-11-node-mongo.appspot.com",
+  messagingSenderId: "747118364107",
+  appId: "1:747118364107:web:e3398e3db28f216fefe24e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
