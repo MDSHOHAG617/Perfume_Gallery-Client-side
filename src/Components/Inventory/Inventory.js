4fca732f0559823aa8f3b0f7f6602730bc46dbd3
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-04-30 21:26:13 +0600

    deliveredupdated!

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -5,11 +5,59 @@ import { Link, useParams } from "react-router-dom";
 const Inventory = () => {
   const { itemsId } = useParams();
   const [items, setItems] = useState({});
+  const [number, setNumber] = useState(1);
+  const [remainingQuantity, setRemainingQuantity] = useState(0);
+  const [updateQuantity, setUpdateQuantity] = useState(0);
+
+  // let count = 0;
+  // const handleDelivered = (id) => {
+  //   let count = number + 1;
+  //   setNumber(count);
+
+  //   fetch(`http://localhost:5000/items/${id}`, {
+  //     method: "PUT",
+  //     headers: {
+  //       "content-type": "application/json",
+  //     },
+  //     body: JSON.stringify(remainingQuantity),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {});
+  // };
+
+  // const handleRestock = (id) => {
+  //   fetch(`http://localhost:5000/items/${id}`, {
+  //     method: "PUT",
+  //     headers: {
+  //       "content-type": "application/json",
+  //     },
+  //     body: JSON.stringify(remainingQuantity),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {});
+
+  // const handleUpdateQuantity = (e) => {
+  //   setUpdateQuantity(e.target.value);
+  // };
+  // const inputValue = parseInt(updateQuantity);
 
   useEffect(() => {
     const url = `http://localhost:5000/items/${itemsId}`;
-    fetch(url).then((res) => res.json().then((data) => setItems(data)));
+    fetch(url).then((res) =>
+      res.json().then((data) => {
+        setItems(data);
+        // const intQuantity = parseInt(data.Quantity);
+        // // const updateQnty = parseInt(updateQuantity);
+        // // console.log();
+        // const remain = intQuantity + inputValue;
+        // setRemainingQuantity(remain);
+        // const remaining = Math.abs(intQuantity - number);
+        // setRemainingQuantity(remaining);
+      })
+    );
   }, []);
+
+  // number, inputValue
 
   const handleForm = (event) => {
     event.preventDefault();
@@ -32,7 +80,12 @@ const Inventory = () => {
             <p className="">
               Stock:{items.Quantity} <span className="text-muted">/left</span>
             </p>
-            <button className="btn btn-primary">Delivered</button>
+            <button
+              // onClick={handleDelivered(items._id)}
+              className="btn btn-primary"
+            >
+              Delivered
+            </button>
           </Card.Body>
         </Card>
         <img className=" img-fluid  " src={items.img} alt="" />
@@ -41,12 +94,18 @@ const Inventory = () => {
         <form onSubmit={handleForm}>
           <h3>Restock the items</h3>
           <input
+            // onBlur={handleUpdateQuantity}
             className="p-1 border-2 rounded bg-light"
             type="number"
             placeholder="insert items (Number) "
           />
           <br />
-          <button className="btn btn-primary btn-sm mt-2">restock</button>
+          <button
+            // onClick={handleRestock(items._id)}
+            className="btn btn-primary btn-sm mt-2"
+          >
+            restock
+          </button>
         </form>
       </div>
 
